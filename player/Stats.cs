8835e5d88069bb9a2500ef8db5270cc87d3112aa
gui.bertoso <154436826+gui-bertoso@users.noreply.github.com>
2026-02-26 02:03:00 -0300

    Initial commit

---
+++ player/Stats.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System;
+
+public partial class Stats : Node
+{
+	public int BaseHealth = 10;
+	public int BaseMoveSpeed = 10;
+	public int ExtraHealth = 0;
+	public int ExtraMoveSpeed = 0;
+
+	public int Health;
+	public int MoveSpeed;
+
+	public override void _Ready()
+	{
+		if (Globals.DevModeEnabled)
+		{
+			SetDevStats()
+		}
+		UpdateStats();
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+
+	public void UpdateStats()
+	{
+		Health = BaseHealth + ExtraHealth;
+		MoveSpeed = BaseMoveSpeed + ExtraMoveSpeed;
+	}
+
+	public void SetDevStats()
+	{
+		ExtraHealth = 1200;
+		ExtraMoveSpeed = 250;
+	}
+}
