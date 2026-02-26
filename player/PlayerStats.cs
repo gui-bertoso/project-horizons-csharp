b43b5f8793bd9cde1820cca6b727893cbddcf318
gui.bertoso <154436826+gui-bertoso@users.noreply.github.com>
2026-02-26 08:05:54 -0300

    feat: port base project files and updates

--- player/PlayerStats.cs
+++ player/PlayerStats.cs
@@ -5,11 +5,26 @@ public partial class PlayerStats : Node
 {
 	public int BaseHealth = 10;
 	public int BaseMoveSpeed = 10;
+
 	public int ExtraHealth = 0;
 	public int ExtraMoveSpeed = 0;
 
+	public float BaseDashCooldown = 2.5f;
+	public float ExtraDashCooldown = 0;
+
+	public float BaseDashDuration = 0.1f;
+	public float ExtraDashDuration = 0;
+
+	public int BaseDashSpeed = 1000;
+	public int ExtraDashSpeed = 0;
+
+	
+
 	public int Health;
 	public int MoveSpeed;
+	public float DashDuration;
+	public float DashCooldown;
+	public int DashSpeed;
 
 	public override void _Ready()
 	{
@@ -28,11 +43,16 @@ public partial class PlayerStats : Node
 	{
 		Health = BaseHealth + ExtraHealth;
 		MoveSpeed = BaseMoveSpeed + ExtraMoveSpeed;
+		DashDuration = BaseDashDuration + ExtraDashDuration;
+		DashCooldown = BaseDashCooldown + ExtraDashCooldown;
+		DashSpeed = BaseDashSpeed + ExtraDashSpeed;
 	}
 
 	public void SetDevStats()
 	{
 		ExtraHealth = 1200;
 		ExtraMoveSpeed = 350;
+		ExtraDashCooldown = -1.2f;
+		ExtraDashDuration = 0.5f;
 	}
 }
