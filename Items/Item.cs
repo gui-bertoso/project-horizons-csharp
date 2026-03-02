ac5bf7df22c41770f6f233eab43d312d4a6bf609
gui.bertoso <154436826+gui-bertoso@users.noreply.github.com>
2026-03-02 01:55:24 -0300

    chore: add miscellaneous improvements and optimizations

---
+++ Items/Item.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+public partial class Item : Resource
+{
+	public enum ITEM_TYPE
+	{
+		Weapon,
+		HeadArmor,
+		BodyArmor,
+		FootArmor,
+		Acessory,
+		Consumable
+	}
+	public enum ITEM_CLASS
+	{
+		Mellee,
+		Mage,
+		HeavyMellee,
+		Wizard,
+		Ranged,
+		Archer,
+		Bommet
+	}
+
+	[Export]
+	public string ItemName;
+	[Export]
+	public ITEM_TYPE ItemType;
+	[Export]
+	public ITEM_CLASS ItemClass;
+	[Export]
+	public string ItemDescription;
+	[Export]
+	public int ItemAmount = 1;
+	[Export]
+	public CompressedTexture2D ItemTexture;
+	[Export]
+	public PackedScene ItemScene;
+}
